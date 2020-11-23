b94ac8025f8b1afab04d871413a740a0e72912e8
borithhor <hor.borith@realwat.net>
2020-11-23 09:54:57 +0700

    update

--- src/containers/Products/Products.tsx
+++ src/containers/Products/Products.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Container } from "@material-ui/core/";
 
-import useStyles from "./styles";
+// import useStyles from "./styles";
 
 const Products: React.FC = React.memo(() => {
-  const classes = useStyles();
+  // const classes = useStyles();
   return <Container maxWidth="lg">Products</Container>;
 });
 export default Products;
